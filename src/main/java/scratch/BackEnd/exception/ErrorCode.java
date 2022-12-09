61dff680357547b7a5d6f906a02fba53e224f275
han <chos9715@naver.com>
2022-12-09 13:28:08 +0900

    fix : error response 변경

--- src/main/java/scratch/BackEnd/exception/ErrorCode.java
+++ src/main/java/scratch/BackEnd/exception/ErrorCode.java
@@ -6,7 +6,23 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    SURVEY_NOT_FOUND(404, "S001", "설문을 찾을 수 없습니다.");
+    SURVEY_NOT_FOUND(404, "S001", "설문을 찾을 수 없습니다."),
+    PROGRESS_CANNOT_BE_MODIFIED(400, "S002", "진행 중인 설문은 수정이 불가능합니다."),
+    PROGRESS_CANNOT_BE_DELETE(400, "S003", "진행 중인 설문은 삭제가 불가능합니다."),
+    ONLY_PROGRESS_CAN_EARLY_CLOSE(400, "S004", "진행 중인 설문만 조기 종료가 가능합니다."),
+    SURVEY_ALREADY_BEEN_COMPLETED(400, "S005", "이미 종료된 설문입니다."),
+    FIRST_COME_FIRST_SERVED_OVER(400, "S006", "선착순 마감된 설문입니다."),
+    DATE_NOT_AVAILABLE_RESPONSE(400, "S007", "설문 응답 가능한 날짜가 아닙니다."),
+    SURVEY_STATUS_IS_NOT_PROGRESS(400, "S008", "진행 중인 설문이 아닙니다."),
+    EMAIL_ALREADY_ADDED(400, "S009", "이미 추가된 이메일 입니다."),
+    EARLY_CLOSED_SURVEY(400, "S010", "조기 마감된 설문입니다."),
+
+
+
+    USER_NOT_FOUND(404, "U001", "유저를 찾을 수 없습니다"),
+    DOES_NOT_HAVE_PERMISSION(400, "U002", "참여 권한이 없습니다."),
+    UNABLE_TO_ANSWER(400, "U003", "응답할 수 없는 설문입니다.");
+
 
     private int status;
     private String code;
