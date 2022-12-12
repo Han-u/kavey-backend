eb254de4aefc9a997a84ed8e0d1efa79ddff476b
= <sbscsue@gmail.com>
2022-12-12 22:27:11 +0900

    fix : enum(surveyTheme)

--- src/main/java/scratch/BackEnd/type/SurveyTheme.java
+++ src/main/java/scratch/BackEnd/type/SurveyTheme.java
@@ -1,7 +1,7 @@
 package scratch.BackEnd.type;
 
 public enum SurveyTheme {
-
+    chunsik,ryan,jordy,apeach,
     DEFAULT, CHUNSIK, RYAN, JORDY, APEACH //지금 못써용(프론트 이미지 컬러로 매칭 해놓음)
     ,lightgray,lightyellow,lightgreen,lightpink
 
